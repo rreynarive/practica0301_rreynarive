5dc8945584f3f29f762429fe65b245ab0c0f7943
Rosa <rreynarive1@educacion.navarra.es>
2023-01-23 19:38:00 +0100

    no acabado

---
+++ Ejercicio 1.py
@@ -0,0 +1,5 @@
+import datetime
+start_time = datetime.datetime.now()
+[(a, b) for a in (1, 3, 5) for b in (2, 4, 6)] # código de ejemplo
+end_time = datetime.datetime.now()
+print end_time - start_time

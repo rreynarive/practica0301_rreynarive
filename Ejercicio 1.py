3ab38379e914f1837423ad5f8c6b87d1a2f1f018
Rosa <rreynarive1@educacion.navarra.es>
2023-01-31 17:40:01 +0100

    cv

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,15 +1,29 @@
 import datetime
-def factorial_recursiva(num):
-    num = 1
-    if num == 1:
+def fibonacci_recursiva(n):
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
-    else:
-        factorial_recursiva = num - 1 + num + 2
-        return factorial_recursiva
-def factorial_bucle(num):
-   factorial = 1
-   for i in range(1, num+1):
-       factorial = factorial - 1 + factorial + 2
-   return factorial
+    return fibonacci_recursiva(n - 1) + fibonacci_recursiva(n - 2)
 
 
+def fibonacci_bucle(n):
+    a = 0
+    b = 1
+    for k in range(n):
+        c = b + a
+        a = b
+        b = c
+    return a
+
+start_time = datetime.datetime.now()
+fibonacci_recursiva(30)
+end_time = datetime.datetime.now()
+
+print("El tiempo de ejercucion es:", end_time - start_time)
+
+start_time = datetime.datetime.now()
+fibonacci_bucle(30)
+end_time = datetime.datetime.now()
+
+print("El tiempo de ejercucion es:", end_time - start_time)
--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,2 +1,21 @@
+import cProfile
+def fibonacci_recursiva(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci_recursiva(n - 1) + fibonacci_recursiva(n - 2)
+
+
+def fibonacci_bucle(n):
+    a = 0
+    b = 1
+    for k in range(n):
+        c = b + a
+        a = b
+        b = c
+    return a
+
+cProfile.run("fibonacci_recursiva(3)")
 
 

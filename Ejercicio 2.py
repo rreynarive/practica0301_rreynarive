6c18dbdeae6d0b4eff6eb37a8dfe516e65c59959
Rosa <rreynarive1@educacion.navarra.es>
2023-02-01 20:25:50 +0100

    falta ejercicio 2

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,13 +1,16 @@
+nif_dict = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
+           "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
+           "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
+           "18": "H", "19": "L", "20": "C", "21": "K", "22": "E"}
 import csv
-import cProfile
+dicc = {}
+with open('personas.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
 
-def personas50(archivo):
-    file = open(archivo, "r")
-    lineas = file.readlines()
-    file.close()
-    claves = lineas[0][:-1].split(";")
-    return print(claves)
-
-archivo = C:\Users\usuario01\Downloads\personas.csv
-print(personas50(archivo))
-cProfile.run("persona50()")
+def nombrecap(nombre):
+    return nombre.tittle()
+def numeroDNI(num_nif):
+    resto = int(num_nif[0:8]) % 23
+    return print(num_nif[0:8] + nif_dict.get(str(resto)))

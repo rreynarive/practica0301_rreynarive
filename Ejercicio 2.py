08bf0c4fcb3951ce2a01805bec504994e52fd978
Rosa Reyna <rreynarive1@educacion.navarra.es>
2023-03-02 15:01:49 +0100

    fin

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,5 +1,5 @@
 import csv
-import cprofile
+import cProfile
 nif_dict = {"0": "T", "1": "R", "2": "W", "3": "A", "4": "G", "5": "M",
            "6": "Y", "7": "F", "8": "P", "9": "D", "10": "X", "11": "B",
            "12": "N", "13": "J", "14": "Z", "15": "S", "16": "Q", "17": "V",
@@ -34,4 +34,4 @@ def check_file(fichero_50, fichero_1000):
             final.weiterow(linea)
 
 
-cProfile.run("leer_ficheros('fichero_50.csv', fichero_1000.csv')")
+cProfile.run("leer_ficheros('50.csv', '1000.csv')")
